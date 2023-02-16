14425649cc7f25bdd13bcea17f249f0c21200e8b
Dokriseum <dokriseum@pm.me>
2023-02-16 09:28:26 +0100

    Weitere Services für die Benutzeridentifikation implementiert

---
+++ Source/CBD.IdentityService.Port/Services/EmailService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+using CBD.IdentityService.Core.Options;
+using CBD.IdentityService.Core.Services;
+
+using Microsoft.Extensions.Options;
+
+namespace CBD.IdentityService.Port.Services; 
+
+public class EmailService : IEmailService {
+	private readonly EmailHostOptions _emailHostOptions;
+	
+	public EmailService(IOptions<EmailHostOptions> emailHostOptions) {
+		this._emailHostOptions = emailHostOptions.Value;
+	}
+
+	public async Task SendEmailAsync(string to, string subject, string message) {
+		var smtpClient = new SmtpClient {
+			Host = this._emailHostOptions.AuthEmailHost,
+			UseDefaultCredentials = false,
+			Credentials = new NetworkCredential(this._emailHostOptions.AuthEmail, this._emailHostOptions.AuthEmailPassword),
+			Port = this._emailHostOptions.AuthEmailHostPort,
+			EnableSsl = true
+		};
+
+		var mailMessage = new MailMessage(this._emailHostOptions.AuthEmail, to) {
+			Subject = subject,
+			Body = message,
+			BodyEncoding = Encoding.UTF8,
+			IsBodyHtml = true
+		};
+
+		await smtpClient.SendMailAsync(mailMessage);
+	}
+}
---
+++ Source/CBD.IdentityService.Port/Services/MappingService.cs
@@ -0,0 +1,22 @@
+﻿using CBD.IdentityService.Core.DTO;
+using CBD.IdentityService.Core.Services;
+
+using Mapster;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace CBD.IdentityService.Port.Services; 
+
+public class MappingService : IMappingService {
+	private TypeAdapterConfig _typeAdapterConfig;
+	
+	public MappingService() {
+		this._typeAdapterConfig = new TypeAdapterConfig();
+		this._typeAdapterConfig.NewConfig<IdentityUser, UserDTO>()
+			.Map(dest => dest.Username, src => src.UserName);
+	}
+	
+	public TTo? MapTo<TTo>(object? obj) {
+		return obj is null ? default : obj.Adapt<TTo>(this._typeAdapterConfig);
+	}
+}
